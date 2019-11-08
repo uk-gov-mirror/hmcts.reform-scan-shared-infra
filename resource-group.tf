993a1b0da20b908db603b2a1221afc647b766d5c
donatas M <1420191+doncem@users.noreply.github.com>
2019-11-08 14:07:05 +0000

    Add previously deleted resources (#1)

---
+++ resource-group.tf
@@ -0,0 +1,10 @@
+locals {
+  tags = "${merge(var.common_tags, map("Team Contact", "#rbs"))}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.env}"
+  location = "${var.location}"
+
+  tags = "${local.tags}"
+}
