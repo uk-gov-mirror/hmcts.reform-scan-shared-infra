a675529474b0e294405047434f91cbee9d4f505d
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2020-02-07 14:19:17 +0000

    Added scheduled-jobs-alert.tf (#16)
    * Added scheduled-jobs-alert.tf
    
    * Changed time interval to 30 minutes
    
    * Changed time interval to 1 day and a half for handle-rejected-files
    
    * Code review
    
    * Time window for handle-rejected-files set to 25 hours

---
+++ scheduled-jobs-alert.tf
@@ -0,0 +1,65 @@
+module "blob-dispatcher-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform_Blob_Dispatcher_-_BSP"
+  alert_desc = "Triggers when no logs from blob-dispatcher job found within timeframe."
+
+  app_insights_query = "traces | where message startswith 'Started blob-dispatcher job'"
+
+  frequency_in_minutes       = 30
+  time_window_in_minutes     = 30
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan blob-dispatcher scheduled job alert"
+  trigger_threshold_operator = "Equal"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
+
+module "delete-dispatched-files-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform_Delete_Dispatched_Files_-_BSP"
+  alert_desc = "Triggers when no logs from delete-dispatched-files job found within timeframe."
+
+  app_insights_query = "traces | where message startswith 'Started delete-dispatched-files job'"
+
+  frequency_in_minutes       = 120
+  time_window_in_minutes     = 120
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan delete-dispatched-files scheduled job alert"
+  trigger_threshold_operator = "Equal"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
+
+module "handle-rejected-files-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform_Handle_Rejected_Files_-_BSP"
+  alert_desc = "Triggers when no logs from handle-rejected-files job found within timeframe."
+
+  app_insights_query = "traces | where message startswith 'Started handle-rejected-files job'"
+
+  # running every hour and checking for 25 hours time window ensures early alert
+  frequency_in_minutes       = 60
+  # 60 * 25 hours = 1500 min
+  time_window_in_minutes     = 1500
+
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan handle-rejected-files scheduled job alert"
+  trigger_threshold_operator = "Equal"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
