6817a71eb8ce259403d35e956945ae779ff9dc99
aliveni <aliveni.choppa@hmcts.net>
2020-03-11 14:38:31 +0000

    Add alert for New Envelopes scheduled job (#35)

--- scheduled-jobs-alert.tf
+++ scheduled-jobs-alert.tf
@@ -111,3 +111,25 @@ module "delete-rejected-files-alert" {
   trigger_threshold          = 0
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
 }
+
+module "check-new-envelopes-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Check-New-Envelopes"
+  alert_desc = "Triggers when no logs from check-new-envelopes job found within timeframe."
+
+  app_insights_query =  "traces | where message startswith 'Started check-new-envelopes job'"
+
+  frequency_in_minutes       = 60
+  time_window_in_minutes     = 60
+
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan check-new-envelopes scheduled job alert"
+  trigger_threshold_operator = "Equal"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
