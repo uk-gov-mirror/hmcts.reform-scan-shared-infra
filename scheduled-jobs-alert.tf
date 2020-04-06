1324c60ea4a20ee2729c6a5ce692ced04e4514f2
aliveni <aliveni.choppa@hmcts.net>
2020-04-06 10:14:03 +0100

    Add alert for send notifications scheduled job (#41)

--- scheduled-jobs-alert.tf
+++ scheduled-jobs-alert.tf
@@ -133,3 +133,24 @@ module "check-new-envelopes-alert" {
   trigger_threshold          = 0
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
 }
+
+module "send-notifications-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform-Scan-Send-Notifications"
+  alert_desc = "Triggers when no logs from send-notifications job found within timeframe."
+
+  app_insights_query = "traces | where message startswith 'Started send-notifications job'"
+
+  frequency_in_minutes       = 20
+  time_window_in_minutes     = 30
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan send-notifications scheduled job alert"
+  trigger_threshold_operator = "Equal"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
