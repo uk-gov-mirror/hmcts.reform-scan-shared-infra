410e4cc0d233b63145f9fefc9b6a9676aa0b96e2
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-04-14 15:58:25 +0100

    Change AppGW Subnet (#42)

--- state.tf
+++ state.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 data "azurerm_subnet" "subnet_a" {
-  name                 = "core-infra-subnet-0-${var.env}"
+  name                 = "reform-scan"
   virtual_network_name = "core-infra-vnet-${var.env}"
   resource_group_name  = "core-infra-${var.env}"
 }
