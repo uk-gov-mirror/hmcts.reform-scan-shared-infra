26fd26123f2961bea7224d9f34f5f88c272b385d
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-06-16 18:31:34 +0200

    Adjust liveness alert (#55)

--- liveness-alert.tf
+++ liveness-alert.tf
@@ -14,11 +14,11 @@ requests
 EOF
 
   frequency_in_minutes       = 15
-  time_window_in_minutes     = 30
+  time_window_in_minutes     = 16
   severity_level             = "2"
   action_group_name          = "${module.alert-action-group.action_group_name}"
   custom_email_subject       = "Blob Router Service liveness"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = 5
+  trigger_threshold          = 10
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
 }
