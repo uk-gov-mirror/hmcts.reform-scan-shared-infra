c9d0daf923aa3890daeaa63c84524040cef36caa
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-08-24 13:46:42 +0100

    Update private_endpoint.tf

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -18,7 +18,7 @@ resource "azurerm_private_endpoint" "private_endpoint" {
 
   private_service_connection {
     name                           = "${local.account_name}-private-connection"
-    private_connection_resource_id = azurerm_storage_account.storage_account.id
+    private_connection_resource_id = "${azurerm_storage_account.storage_account.id}"
     is_manual_connection           = false
     subresource_names              = ["blob"]
   }
