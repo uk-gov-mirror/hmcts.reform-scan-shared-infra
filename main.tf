38e255a9a84d8e82bd641377bd57cc888e5f6807
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-08-27 11:22:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "mgmt"
-    storage_account_name = "capybaramgmt"
-    container_name       = "terraform"
-    key                  = "reform-scan-shared-infra.tfstate"
-  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
