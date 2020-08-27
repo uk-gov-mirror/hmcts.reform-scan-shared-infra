061b256d7f5ed257be4587e71ab8b28833d89003
James Johnson <jrjohnson87@googlemail.com>
2020-08-27 14:14:11 +0100

    Update to TF 0.13

--- state.tf
+++ state.tf
@@ -1,5 +1,14 @@
 terraform {
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+
   backend "azurerm" {}
+
+  required_version = ">= 0.13"
 }
 
 data "azurerm_subnet" "subnet_a" {
