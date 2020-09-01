f58221265a570a6318f6cbf2e93777b75affcdbd
James Johnson <jrjohnson87@googlemail.com>
2020-09-01 13:53:39 +0100

    Remove AppGW NSG

--- app-gateway-nsg.tf
+++ app-gateway-nsg.tf
@@ -29,49 +29,49 @@ data "azurerm_key_vault_secret" "aks01_public_ip_prefix" {
   key_vault_id = "${data.azurerm_key_vault.reform_scan_key_vault.id}"
 }
 
-resource "azurerm_network_security_group" "reformscannsg" {
-  name                = "reform-scan-nsg-${var.env}"
-  resource_group_name = "core-infra-${var.env}"
-  location            = "${var.location}"
+# resource "azurerm_network_security_group" "reformscannsg" {
+#   name                = "reform-scan-nsg-${var.env}"
+#   resource_group_name = "core-infra-${var.env}"
+#   location            = "${var.location}"
 
-  security_rule {
-    name                       = "allow-inbound-https-external"
-    direction                  = "Inbound"
-    access                     = "Allow"
-    priority                   = 100
-    source_address_prefixes    = ["${split(",", data.azurerm_key_vault_secret.allowed_external_ips.value)}"]
-    source_port_range          = "*"
-    destination_address_prefix = "*"
-    destination_port_range     = "443"
-    protocol                   = "TCP"
-  }
+#   security_rule {
+#     name                       = "allow-inbound-https-external"
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     priority                   = 100
+#     source_address_prefixes    = ["${split(",", data.azurerm_key_vault_secret.allowed_external_ips.value)}"]
+#     source_port_range          = "*"
+#     destination_address_prefix = "*"
+#     destination_port_range     = "443"
+#     protocol                   = "TCP"
+#   }
 
-  security_rule {
-    name                       = "allow-inbound-https-proxyout"
-    direction                  = "Inbound"
-    access                     = "Allow"
-    priority                   = 110
-    source_address_prefixes    = ["${split(",", data.azurerm_public_ip.proxy_out_public_ip.ip_address)}"]
-    source_port_range          = "*"
-    destination_address_prefix = "*"
-    destination_port_range     = "443"
-    protocol                   = "TCP"
-  }
+#   security_rule {
+#     name                       = "allow-inbound-https-proxyout"
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     priority                   = 110
+#     source_address_prefixes    = ["${split(",", data.azurerm_public_ip.proxy_out_public_ip.ip_address)}"]
+#     source_port_range          = "*"
+#     destination_address_prefix = "*"
+#     destination_port_range     = "443"
+#     protocol                   = "TCP"
+#   }
 
-  security_rule {
-    name                       = "allow-inbound-https-internal"
-    direction                  = "Inbound"
-    access                     = "Allow"
-    priority                   = 120
-    source_address_prefixes    = ["${data.azurerm_key_vault_secret.aks00_public_ip_prefix.value}","${data.azurerm_key_vault_secret.aks01_public_ip_prefix.value}"]
-    source_port_range          = "*"
-    destination_address_prefix = "*"
-    destination_port_range     = "443"
-    protocol                   = "TCP"
-  }
-}
+#   security_rule {
+#     name                       = "allow-inbound-https-internal"
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     priority                   = 120
+#     source_address_prefixes    = ["${data.azurerm_key_vault_secret.aks00_public_ip_prefix.value}","${data.azurerm_key_vault_secret.aks01_public_ip_prefix.value}"]
+#     source_port_range          = "*"
+#     destination_address_prefix = "*"
+#     destination_port_range     = "443"
+#     protocol                   = "TCP"
+#   }
+# }
 
-resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
-  subnet_id                 = "${data.azurerm_subnet.subnet_a.id}"
-  network_security_group_id = "${azurerm_network_security_group.reformscannsg.id}"
-}
+# resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+#   subnet_id                 = "${data.azurerm_subnet.subnet_a.id}"
+#   network_security_group_id = "${azurerm_network_security_group.reformscannsg.id}"
+# }
