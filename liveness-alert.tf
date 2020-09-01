2948b7dafde9612b7ae7e51eb321d92a6ed1e7af
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2020-09-01 18:05:17 +0100

    Notification service liveness alert (#71)

--- liveness-alert.tf
+++ liveness-alert.tf
@@ -9,7 +9,7 @@ module "blob-router-service-liveness-alert" {
 
   app_insights_query = <<EOF
 requests
-| where name == "GET /health" and resultCode != "200"
+| where url endswith "/health/liveness" and success != "True"
 | where cloud_RoleName == "Blob Router Service"
 EOF
 
@@ -18,6 +18,31 @@ EOF
   severity_level             = "2"
   action_group_name          = "${module.alert-action-group.action_group_name}"
   custom_email_subject       = "Blob Router Service liveness"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = 10
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
+
+module "reform-scan-notification-service-liveness-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform_Scan_Notification_Service_liveness"
+  alert_desc = "Triggers when reform scan notification service looks like being down within a 30 minutes window timeframe."
+
+  app_insights_query = <<EOF
+requests
+| where url endswith "/health/liveness" and success != "True"
+| where cloud_RoleName == "Reform Scan Notification Service"
+EOF
+
+  frequency_in_minutes       = 15
+  time_window_in_minutes     = 16
+  severity_level             = "2"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan Notification Service liveness"
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold          = 10
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
