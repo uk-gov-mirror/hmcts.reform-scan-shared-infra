fa04261f78c8993f17fd8721c13d82afcc73dfe7
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-06 21:38:34 +0100

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,6 +1,6 @@
 module "vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = "${var.product}-${var.env}"
+  name                    = "${var.product}-${var.env}-kv"
   product                 = "${var.product}"
   env                     = "${var.env}"
   tenant_id               = "${var.tenant_id}"
