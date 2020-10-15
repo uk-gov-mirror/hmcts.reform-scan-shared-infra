878f0684d0d9473e6b05f8f81212a701d377de2b
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-15 22:17:12 +0100

    Update private_endpoint-staging.tf

--- private_endpoint-staging.tf
+++ private_endpoint-staging.tf
@@ -1,11 +1,11 @@
 resource "azurerm_template_deployment" "private_endpoint_staging" {
-  name                = "${local.account_name}-endpoint"
+  name                = "${local.account_name}stg-endpoint"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
   template_body = file("private_endpoint_template.json")
 
   parameters = {
-    endpoint_name       = "${local.account_name}-endpoint"
+    endpoint_name       = "${local.account_name}stg-endpoint"
     endpoint_location   = "${azurerm_resource_group.rg.location}"
     subnet_id           = "${data.azurerm_subnet.scan_storage_subnet.id}"
     storageaccount_id   = "${azurerm_storage_account.storage_account_staging.id}" 
