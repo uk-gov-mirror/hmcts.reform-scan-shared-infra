424bfa29a2cc13be68d564f60f7598e8a0d9c1ac
aliveni <aliveni.choppa@hmcts.net>
2020-12-07 17:19:31 +0000

    Add alert for no envelopes processed in PCQ container (#75)

--- no-envelopes-processed-alert.tf
+++ no-envelopes-processed-alert.tf
@@ -45,3 +45,27 @@ EOF
   trigger_threshold           = 0
   resourcegroup_name          = "${azurerm_resource_group.rg.name}"
 }
+
+module "no-pcq-envelopes-processed-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled     = "${var.env == "prod"}"
+  alert_name  = "No_pcq_envelopes_processed_-_Blob_Router"
+  alert_desc  = "Triggers when Blob Router did not process single PCQ envelope in last hour within SLA."
+
+  app_insights_query = <<EOF
+traces
+| where message startswith "No Envelopes created in PCQ"
+EOF
+
+  frequency_in_minutes        = 60
+  time_window_in_minutes      = 60
+  severity_level              = "4"
+  action_group_name           = "${module.alert-action-group.action_group_name}"
+  custom_email_subject        = "Blob Router - No PCQ envelopes processed"
+  trigger_threshold_operator  = "GreaterThan"
+  trigger_threshold           = 0
+  resourcegroup_name          = "${azurerm_resource_group.rg.name}"
+}
