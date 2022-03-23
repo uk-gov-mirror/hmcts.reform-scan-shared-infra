bc273494e9f35a6e5941e278d60317698fbb64de
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-03-23 03:46:08 +0000

    DTSPO-4471 - add private endpoint dns (#107)

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -4,6 +4,12 @@ locals {
   scan_storage_vnet_subnet_name    = "scan-storage"
 }
 
+data "azurerm_private_dns_zone" "private_link_dns_zone" {
+  provider            = azurerm.mgmt
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = "core-infra-intsvc-rg"
+}
+
 data "azurerm_subnet" "scan_storage_subnet" {
   name                 = local.scan_storage_vnet_subnet_name
   virtual_network_name = local.scan_storage_vnet_name
@@ -21,5 +27,10 @@ resource "azurerm_private_endpoint" "private_endpoint" {
     private_connection_resource_id = azurerm_storage_account.storage_account.id
     is_manual_connection           = false
     subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = data.azurerm_private_dns_zone.private_link_dns_zone.name
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.private_link_dns_zone.id]
   }
 }
