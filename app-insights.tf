dd166b363b212099363c8d13d9991af474107217
MUSTAFA <mdayican@gmail.com>
2022-09-09 16:10:39 +0100

    add insights-connection to key vault for appinsight 3.x upgrade (#123)

--- app-insights.tf
+++ app-insights.tf
@@ -4,6 +4,12 @@ resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
   key_vault_id = module.vault.key_vault_id
 }
 
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.vault.key_vault_id
+}
+
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
   location            = var.location
