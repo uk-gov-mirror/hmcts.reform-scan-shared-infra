cae1f27a2f2566fa00d9cd3cf574a41af317b170
Daniel Furnivall <danielfurnivall@gmail.com>
2025-02-18 18:16:48 +0000

    changed files to use https

--- scheduled-jobs-alert.tf
+++ scheduled-jobs-alert.tf
@@ -1,5 +1,5 @@
 module "blob-dispatcher-alert" {
-  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  source            = "git::https://github.com/hmcts/cnp-module-metric-alert.git"
   location          = var.location
   app_insights_name = module.application_insights.name
 
