b44cae811a3c734453a0f7f3f258cf8c4332a52f
Danny Furnivall <danielfurnivall@gmail.com>
2025-02-18 14:48:44 +0000

    Update queue-premium.tf

--- queue-premium.tf
+++ queue-premium.tf
@@ -9,7 +9,6 @@ module "queue-namespace-premium" {
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = var.sku_service_bus
   capacity            = var.capacity_service_bus
-  zone_redundant      = var.zone_redundant_service_bus
   env                 = var.env
   common_tags         = local.tags
 }
