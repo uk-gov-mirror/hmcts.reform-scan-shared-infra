1e31677220954f1d28a570a23f92ee4abcda5744
Imran Ali <101260174+justiceia@users.noreply.github.com>
2025-07-24 16:40:43 +0100

    FACT-1731 private endpoint depends on storage account existing first (#152)
    * FACT-1731 private endpoint depends on storage account existing first
    
    * comment out private endpoint and network rules

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -16,21 +16,21 @@ data "azurerm_subnet" "scan_storage_subnet" {
   resource_group_name  = local.scan_storage_vnet_resource_group
 }
 
-resource "azurerm_private_endpoint" "private_endpoint" {
-  name                = "${local.account_name}-endpoint"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = data.azurerm_subnet.scan_storage_subnet.id
-
-  private_service_connection {
-    name                           = "${local.account_name}-endpoint"
-    private_connection_resource_id = azurerm_storage_account.storage_account.id
-    is_manual_connection           = false
-    subresource_names              = ["blob"]
-  }
-
-  private_dns_zone_group {
-    name                 = data.azurerm_private_dns_zone.private_link_dns_zone.name
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.private_link_dns_zone.id]
-  }
-}
+# resource "azurerm_private_endpoint" "private_endpoint" {
+#   name                = "${local.account_name}-endpoint"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = data.azurerm_subnet.scan_storage_subnet.id
+#
+#   private_service_connection {
+#     name                           = "${local.account_name}-endpoint"
+#     private_connection_resource_id = azurerm_storage_account.storage_account.id
+#     is_manual_connection           = false
+#     subresource_names              = ["blob"]
+#   }
+#
+#   private_dns_zone_group {
+#     name                 = data.azurerm_private_dns_zone.private_link_dns_zone.name
+#     private_dns_zone_ids = [data.azurerm_private_dns_zone.private_link_dns_zone.id]
+#   }
+# }
